bada3dea074eff46e86b565bbd8d05f1b29ddb66
ridenaio <50695987+ridenaio@users.noreply.github.com>
2021-07-06 17:23:24 +0500

    Add auto check for API key expiration (#14)

--- pages/api/key/check.js
+++ pages/api/key/check.js
@@ -1,11 +1,21 @@
 import {query as q} from 'faunadb'
 import {serverClient} from '../../../shared/utils/faunadb'
+import {getEpoch} from '../../../shared/utils/node-api'
+
+const KEY_EXPIRATION_TIMEOUT_MIN = parseInt(process.env.KEY_EXPIRATION_TIMEOUT_MIN || 7 * 24 * 60)
 
 export default async (req, res) => {
   const {key} = req.query
   if (!key) {
     return res.status(400).send('bad request')
   }
+
+  const {epoch, nextValidation} = await getEpoch()
+
+  const expirationDt = new Date(nextValidation)
+
+  expirationDt.setMinutes(expirationDt.getMinutes() - KEY_EXPIRATION_TIMEOUT_MIN)
+
   try {
     const query = await serverClient.query(
       q.Let(
@@ -27,8 +37,10 @@ export default async (req, res) => {
         )
       )
     )
-    if (query.result && query.result.epoch < parseInt(process.env.MIN_AVAILABLE_EPOCH))
+
+    if (query.result && query.result.epoch < epoch && new Date() > expirationDt) {
       return res.status(400).send('API key is expired. Try to buy another one.')
+    }
     return res.json(query.result)
   } catch (e) {
     return res.status(400).send('failed to retrieve api key')
