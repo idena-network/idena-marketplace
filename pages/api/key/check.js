a19fa7ba05905d3db105964d893f507a3c4d7731
ridenaio <50695987+ridenaio@users.noreply.github.com>
2021-07-23 14:11:04 +0500

    Remove expiration check (#15)

--- pages/api/key/check.js
+++ pages/api/key/check.js
@@ -1,20 +1,11 @@
 import {query as q} from 'faunadb'
 import {serverClient} from '../../../shared/utils/faunadb'
-import {getEpoch} from '../../../shared/utils/node-api'
-
-const KEY_EXPIRATION_TIMEOUT_MIN = parseInt(process.env.KEY_EXPIRATION_TIMEOUT_MIN || 7 * 24 * 60)
 
 export default async (req, res) => {
   const {key} = req.query
   if (!key) {
     return res.status(400).send('bad request')
   }
-
-  const {epoch, nextValidation} = await getEpoch()
-
-  const expirationDt = new Date(nextValidation)
-
-  expirationDt.setMinutes(expirationDt.getMinutes() - KEY_EXPIRATION_TIMEOUT_MIN)
 
   try {
     const query = await serverClient.query(
@@ -31,16 +22,10 @@ export default async (req, res) => {
               epoch: q.Select(['data', 'epoch'], q.Get(q.Var('ref'))),
             },
           },
-          {
-            result: null,
-          }
+          q.Abort('key not found')
         )
       )
     )
-
-    if (query.result && query.result.epoch < epoch && new Date() > expirationDt) {
-      return res.status(400).send('API key is expired. Try to buy another one.')
-    }
     return res.json(query.result)
   } catch (e) {
     return res.status(400).send('failed to retrieve api key')
